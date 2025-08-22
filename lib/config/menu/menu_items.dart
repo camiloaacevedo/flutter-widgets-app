c573d4eba158c5efe82bb0263443279426a65993
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-08-21 20:11:44 -0500

    ui screen terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -48,9 +48,16 @@ const appMenuItems = <MenuItem>[
 
   MenuItem(
     title: 'Animated container',
-    subTitle: 'Statefull widget animado',
+    subTitle: 'Stateful widget animado',
     link: '/animated',
     icon: Icons.check_box_outline_blank_outlined
+  ),
+
+  MenuItem(
+    title: 'UI Controls + Tiles',
+    subTitle: 'Una serie don controles de Flutter',
+    link: '/ui-controls',
+    icon: Icons.car_rental_outlined
   ),
 
 
